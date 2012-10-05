d90d60b988c5bbccf1c65fc7bb51c6721daf3d14
Roland Walker <walker@pobox.com>
2012-10-05 19:38:38 -0400

    require updated persistent-soft

--- ucs-utils.el
+++ ucs-utils.el
@@ -8,7 +8,7 @@
 ;; Version: 0.7.0
 ;; Last-Updated: 6 Sep 2012
 ;; EmacsWiki: UcsUtils
-;; Package-Requires: ((persistent-soft "0.8.0") (pcache "0.2.3"))
+;; Package-Requires: ((persistent-soft "0.8.6") (pcache "0.2.3"))
 ;; Keywords: i18n, extensions
 ;;
 ;; Simplified BSD License
