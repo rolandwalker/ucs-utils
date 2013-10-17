4b723d9984eb08d8f61c13e6bf52499bcd74a311
Roland Walker <walker@pobox.com>
2013-10-17 11:01:41 -0400

    bump version

--- ucs-utils.el
+++ ucs-utils.el
@@ -5,8 +5,8 @@
 ;; Author: Roland Walker <walker@pobox.com>
 ;; Homepage: http://github.com/rolandwalker/ucs-utils
 ;; URL: http://raw.github.com/rolandwalker/ucs-utils/master/ucs-utils.el
-;; Version: 0.7.2
-;; Last-Updated:  5 Oct 2012
+;; Version: 0.7.4
+;; Last-Updated: 17 Oct 2013
 ;; EmacsWiki: UcsUtils
 ;; Package-Requires: ((persistent-soft "0.8.6") (pcache "0.2.3"))
 ;; Keywords: i18n, extensions
@@ -164,7 +164,7 @@
 ;;;###autoload
 (defgroup ucs-utils nil
   "Utilities for Unicode characters."
-  :version "0.7.2"
+  :version "0.7.4"
   :link '(emacs-commentary-link :tag "Commentary" "ucs-utils")
   :link '(url-link :tag "GitHub" "http://github.com/rolandwalker/ucs-utils")
   :link '(url-link :tag "EmacsWiki" "http://emacswiki.org/emacs/UcsUtils")
